6c1b58919af66ea580f5b03063175fb19eab3882
shanta1996 <shantachaudhary490@gmail.com>
2024-12-04 11:49:39 +0545

    changes

--- frontend/ebook-library/src/components/FrontHome.jsx
+++ frontend/ebook-library/src/components/FrontHome.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FrontHome = () => {
   return (
     <>
-    <div className='grid grid-cols-1 md:grid-cols-2 justify-items-center gap-[100px] px-5 md:px-[100px] h-full'>
+    <div className='pt-[200px] grid grid-cols-1 md:grid-cols-2 justify-items-center gap-[100px] px-5 md:px-[100px] h-full'>
       <div className='pt-[100px] h-full '>
         <h1 className='text-xl xl:text-2xl font-semibold'>E-BOOK LIBRARY</h1>
         <p className='text-[18px]'>E-Pustakalaya is a free education-focused digital library containing full-text documents, images, videos and audio files that can be accessed through an intranet or the internet. E-Pustakalaya users can read books, view videos, learn through interactive activities and listen to audio clips. E-Pustakalaya holds collections in Nepali, English and other languages of Nepal.</p>
--- frontend/ebook-library/src/components/Navbar.jsx
+++ frontend/ebook-library/src/components/Navbar.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <>
-      <nav className='bg-slate p-6 w-[100%] md:block hidden'>
+    <div className='fixed left-0 right-0 top-0'>
+      <nav className=' bg-slate p-6 w-[100%] md:block hidden'>
         <ul className='flex items-center justify-evenly w-[100%]'>
           <Link to="" className='flex gap-[10px] font-semibold text-[35px] 2xl:text-[40px]  '>
             <img className='h-[50px]' src="/logo.png" alt="logo" />
@@ -19,12 +20,10 @@ const Navbar = () => {
           <li className='py-2 px-3 border-2 border-solid border-black rounded-md ease-linear duration-[0.1s] hover:border-[3px]'>
           <Link  to="">LOGIN</Link>
           </li>
-          
-          
         </ul>
       </nav>
 
-      <div className=' p-6 border-b-2 shadow-sm shadow-slate-100'>
+      <div className='bg-white p-6 border-b-2 shadow-sm shadow-slate-100'>
         <ul className='flex items-center text-md gap-[100px] justify-center'>
 
           <li>
@@ -39,7 +38,7 @@ const Navbar = () => {
 
         </ul>
       </div>
-      
+      </div>
     </>
   )
 }
